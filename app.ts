bd857e5500852f5df343373791d75735fa4ebb9f
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-08-18 16:54:39 +0200

    first few lessons

--- app.ts
+++ app.ts
@@ -1,3 +1,12 @@
-const hello : string = 'womble'
+function add(n1: number, n2: number, showRes: boolean, resultIs : string){
+  if (showRes) {
+    console.log(message, n1 + n2);
+  }
+}
 
-console.log(hello)
+const number1 = 2.9;
+const number2 = 33;
+const printRes = true;
+const message = 'result is: '
+
+add(number1, number2, printRes, message);
