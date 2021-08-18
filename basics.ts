eabe48a8ba3c8ac22dade7aa26f36b898af78ddd
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-08-18 18:47:59 +0200

    a few more lessons on basic types up to union types

---
+++ basics.ts
@@ -0,0 +1,12 @@
+function add(n1: number, n2: number, showRes: boolean, resultIs : string){
+  if (showRes) {
+    console.log(message, n1 + n2);
+  }
+}
+
+const number1 = 2.9;
+const number2 = 33;
+const printRes = true;
+const message = 'result is: '
+
+add(number1, number2, printRes, message);
